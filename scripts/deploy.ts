124903c937a5dbf1b0eaac2ed51fa361de4a9f3f
An-Ting Hsu <andyhsu10@gmail.com>
2023-02-25 01:39:17 +0800

    feat: local testnode command

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -7,12 +7,22 @@ async function main() {
 
   const lockedAmount = ethers.utils.parseEther('1')
 
+  const latestBlock = await ethers.provider.getBlock('latest')
+  console.log(`Latest block number: ${latestBlock.number}`)
+  console.log(`Latest block timestamp: ${latestBlock.timestamp}`)
+  console.log(`Current timestamp: ${currentTimestampInSeconds}`)
+  
   const Lock = await ethers.getContractFactory('Lock')
   const lock = await Lock.deploy(unlockTime, { value: lockedAmount })
 
+  const Nft = await ethers.getContractFactory('NFT')
+  const nft = await Nft.deploy('Account Bond NFTs', 'ABN', 'https://1ton-labs.vercel.app/api/metadata/')
+
   await lock.deployed()
+  await nft.deployed()
 
   console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`)
+  console.log(`Account Bond NFT deployed to ${nft.address}`)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
