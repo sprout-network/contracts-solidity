32b840b58215c3a55f7c00d6b2616769d2e590f6
daiwanwei <93238466+daiwanwei@users.noreply.github.com>
2023-02-24 15:08:10 +0800

    setting mainnet fork in local testing (#4)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,18 @@
 import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from 'dotenv'
+dotenv.config()
+
 
 const config: HardhatUserConfig = {
     solidity: "0.8.17",
     networks: {
         hardhat: {
             allowUnlimitedContractSize: false,
+            forking: {
+                url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+                // blockNumber: 16696079
+            }
         },
         mainnet: {
             url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
