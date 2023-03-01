ca395ab704b46f7df130fb30f3c7681ce02ab6f9
daiwanwei <wadejet.work@gmail.com>
2023-03-01 11:14:55 +0800

    update nftfi unit test

--- test/NFTfi.ts
+++ test/NFTfi.ts
@@ -1,42 +1,73 @@
 import { time, loadFixture } from '@nomicfoundation/hardhat-network-helpers'
 import { expect } from 'chai'
 import { ethers } from 'hardhat'
-import { generateBorrowerHash, generateLenderHash } from '../helpers/order'
 import { BigNumber } from 'ethers'
+import { addNftToWhitelist, signOrder } from './helpers/nftfi'
 
-describe('NFTfi', function () {
+describe('NFTfi', function() {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
   async function connectNFTfiFixture() {
     //NFTfi contract address (https://github.com/NFTfi-Genesis/nftfi.eth/tree/main/V1)
-    const addr = '0x88341d1a8f672d2780c8dc725902aae72f143b0c'
+    const nftfiAddr = '0x88341d1a8f672d2780c8dc725902aae72f143b0c'
+    const nftfiOwnerAddr = '0xDcA17eeDc1aa3dbB14361678566b2dA5A1Bb4C31'
+    const nftfiOwner = await ethers.getImpersonatedSigner(nftfiOwnerAddr)
+
     // Contracts are deployed using the first signer/account by default
     const [owner, otherAccount] = await ethers.getSigners()
-    const nftfi = await ethers.getContractAt('INFTfi', addr)
-    return { nftfi, owner, otherAccount }
+    await owner.sendTransaction({
+      to: nftfiOwner.address,
+      value: ethers.utils.parseEther('10.0') // Sends exactly 1.0 ether
+    })
+    const nftfi = await ethers.getContractAt('INFTfi', nftfiAddr)
+    return { nftfi, nftfiOwner, owner, otherAccount }
   }
 
-  describe('NFTfiAdmin', function () {
-    it('Should be the right name and symbol', async function () {
+  describe('NFTfiAdmin', function() {
+    it('Should be the right name and symbol', async function() {
       const { nftfi } = await loadFixture(connectNFTfiFixture)
       const name = 'NFTfi Promissory Note'
       const symbol = 'NFTfi'
       expect(await nftfi.name()).to.equal(name)
       expect(await nftfi.symbol()).to.equal(symbol)
     })
+    it('Should be the right whitelist of erc20', async function() {
+      const { nftfi } = await loadFixture(connectNFTfiFixture)
+      const whitelist = [
+        '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', //WETH
+        '0x6B175474E89094C44Da98b954EedeAC495271d0F' //DAI
+      ]
+      for (const w of whitelist) {
+        expect(await nftfi.erc20CurrencyIsWhitelisted(w)).to.equal(true)
+      }
+    })
+
+    it('Should be the right owner', async function() {
+      const { nftfi, nftfiOwner } = await loadFixture(connectNFTfiFixture)
+      const owner = await nftfi.owner()
+      expect(owner).to.equal(nftfiOwner.address)
+    })
+
+    it('Should be the right owner', async function() {
+      const { nftfi, nftfiOwner } = await loadFixture(connectNFTfiFixture)
+      const nftAddr = '0x0c60b40289ff15ff6afdfa668d1a743dc6e53cf3'
+      expect(await nftfi.nftContractIsWhitelisted(nftAddr)).to.equal(false)
+      await addNftToWhitelist(nftfi.address, nftfiOwner.address, nftAddr)
+      expect(await nftfi.nftContractIsWhitelisted(nftAddr)).to.equal(true)
+    })
   })
 
-  describe('NFTfiSigningUtils', function () {
-    it('Should be the right borrower sig', async function () {
+  describe('NFTfiSigningUtils', function() {
+    it('Should be the right borrower sig', async function() {
       const { nftfi, owner } = await loadFixture(connectNFTfiFixture)
       const chainId = await nftfi.getChainID()
       const nftCollateralContract = '0xB75F09b4340aEb85Cd5F2Dd87d31751EDC11ed39'
       const borrower = owner.address
       const nftCollateralId = 1n
       const borrowerNonce = 1n
-      const hash = generateBorrowerHash({ chainId, nftCollateralContract, borrower, nftCollateralId, borrowerNonce })
-      const sig = await owner.signMessage(hash)
+      const order = { chainId, nftCollateralContract, borrower, nftCollateralId, borrowerNonce }
+      const sig = signOrder(owner, 'BORROW', order)
       const isValid = await nftfi.isValidBorrowerSignature(
         nftCollateralId,
         borrowerNonce,
@@ -46,7 +77,7 @@ describe('NFTfi', function () {
       )
       expect(isValid).to.true
     })
-    it('Should be the right lender sig', async function () {
+    it('Should be the right lender sig', async function() {
       const { nftfi, owner } = await loadFixture(connectNFTfiFixture)
       const chainId = await nftfi.getChainID()
       const loanPrincipalAmount = 100n
@@ -60,7 +91,7 @@ describe('NFTfi', function () {
       const lender = owner.address
       const nftCollateralId = BigNumber.from(1)
       const lenderNonce = BigNumber.from(1)
-      const hash = generateLenderHash({
+      const order = {
         loanPrincipalAmount,
         maximumRepaymentAmount,
         nftCollateralId,
@@ -72,9 +103,9 @@ describe('NFTfi', function () {
         loanERC20Denomination,
         lender,
         interestIsProRated,
-        chainId,
-      })
-      const sig = await owner.signMessage(hash)
+        chainId
+      }
+      const sig = signOrder(owner, 'LEND', order)
       const isValid = await nftfi.isValidLenderSignature(
         loanPrincipalAmount,
         maximumRepaymentAmount,
