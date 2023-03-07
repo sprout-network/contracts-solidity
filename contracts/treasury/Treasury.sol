20c7868d937fa678ab252b3e627dc3f7ac3b2aeb
e24016611 <sunboyboy1@gmail.com>
2023-03-07 21:53:10 +0800

    Add Whitelist Withdraw (#13)
    * add white list withdraw
    
    * stored used nonce

--- contracts/treasury/Treasury.sol
+++ contracts/treasury/Treasury.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.8.0;
 
 import '@openzeppelin/contracts/interfaces/IERC20.sol';
 import '@openzeppelin/contracts/interfaces/IERC721.sol';
+import '@openzeppelin/contracts/utils/cryptography/ECDSA.sol';
 import './TreasuryAdmin.sol';
 
 contract Treasury is TreasuryAdmin {
@@ -18,15 +19,24 @@ contract Treasury is TreasuryAdmin {
 
     event Withdraw(uint256 indexed poolId, address indexed to, address indexed coin, uint256 amount);
 
+    event WhitelistWithdraw(address indexed to, address indexed coin, uint256 amount);
+
     mapping(address => mapping(uint256 => uint256)) public getPoolId;
 
     mapping(uint256 => PoolInfo) public getPoolInfo;
 
     mapping(uint256 => bool) public isActive;
 
-    uint256 public totalNumPool = 0;
+    mapping(uint256 => bool) public isNonceUsed;
 
-    constructor(address[] memory _coins) TreasuryAdmin(_coins) {}
+    uint256 public totalNumPool = 0;
+    address private _signer;
+
+    constructor(address[] memory _coins) TreasuryAdmin(_coins) {
+        _signer = owner();
+    }
+
+    using ECDSA for bytes32;
 
     function createPool(
         address _nftAddress,
@@ -64,6 +74,32 @@ contract Treasury is TreasuryAdmin {
         getPoolInfo[_poolId].balances -= _amount;
         emit Withdraw(_poolId, msg.sender, _coin, _amount);
         return true;
+    }
+
+    function whitelistWithdraw(
+        address _coin,
+        uint256 _amount,
+        uint256 _nonce,
+        bytes memory _depositProof
+    ) external returns(bool){
+        require(_isValidWithdrawSignature(msg.sender, _coin, _amount, _nonce, _depositProof), 'invalid deposit proof');
+        require(!isNonceUsed[_nonce], 'nonce has been used');
+        require(IERC20(_coin).transfer(msg.sender, _amount), 'transfer coin fail');
+        isNonceUsed[_nonce] = true;
+        emit WhitelistWithdraw(msg.sender, _coin, _amount);
+        return true;
+    }
+
+    function _isValidWithdrawSignature(
+        address _receiver,
+        address _coin,
+        uint256 _amount,
+        uint256 _nonce,
+        bytes memory _depositProof
+    ) private view returns(bool){
+        bytes32 message = keccak256(abi.encodePacked(_receiver, _coin, _amount, _nonce));
+        bytes32 messageWithEthSignPrefix = message.toEthSignedMessageHash();
+        return (messageWithEthSignPrefix.recover(_depositProof) == _signer);
     }
 
     modifier hasOpened(uint256 _poolId) {
