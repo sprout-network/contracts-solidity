155052431fd506b0d5ebfaa38551e76c445a2578
daiwanwei <wadejet.work@gmail.com>
2023-03-08 10:09:51 +0800

    remove Lock contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,19 +1,6 @@
 import { ethers } from 'hardhat'
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000)
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS
-
-  const lockedAmount = ethers.utils.parseEther('1')
-
-  const latestBlock = await ethers.provider.getBlock('latest')
-  console.log(`Latest block number: ${latestBlock.number}`)
-  console.log(`Latest block timestamp: ${latestBlock.timestamp}`)
-  console.log(`Current timestamp: ${currentTimestampInSeconds}`)
-
-  const Lock = await ethers.getContractFactory('Lock')
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount })
 
   const Nft = await ethers.getContractFactory('NFT')
   const nft = await Nft.deploy('Account Bond NFTs', 'ABN', 'https://1ton-labs.vercel.app/api/metadata/')
@@ -21,11 +8,9 @@ async function main() {
   const NFTfi = await ethers.getContractFactory('NFTfi')
   const nftfi = await NFTfi.deploy()
 
-  await lock.deployed()
   await nft.deployed()
   await nftfi.deployed()
 
-  console.log(`\x1b[31mLock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}\x1b[0m`)
   console.log(`\x1b[31mAccount Bond NFT deployed to ${nft.address}\x1b[0m`)
   console.log(`\x1b[31mAccount NFTfi deployed to ${nftfi.address}\x1b[0m`)
 
